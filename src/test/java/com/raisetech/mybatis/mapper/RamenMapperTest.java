fa1ff80fc438ad03aa345e03b5cc16c8583bee57
mae <dup0729@gmail.com>
2023-04-12 11:49:01 +0900

    testコード作成

---
+++ src/test/java/com/raisetech/mybatis/mapper/RamenMapperTest.java
@@ -0,0 +1,73 @@
+package com.raisetech.mybatis.mapper;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.spring.api.DBRider;
+import com.raisetech.mybatis.entity.Ramen;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@DBRider
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class RamenMapperTest {
+
+  @Autowired
+  RamenMapper ramenMapper;
+
+  @Test
+  @DataSet(value = {"datasets/ramen.yml"})
+  @Transactional
+  public void findallで全データが取得されること() {
+    List<Ramen> ramens = ramenMapper.findAll();
+    List<Ramen> ramenList = List.of(ramens.get(0), ramens.get(1), ramens.get(2), ramens.get(3), ramens.get(4), ramens.get(5), ramens.get(6), ramens.get(7), ramens.get(8), ramens.get(9));
+    assertThat(ramens).hasSize(10).isEqualTo(ramenList);
+  }
+
+  @Test
+  @DataSet(value = {"datasets/ramen.yml"})
+  @Transactional
+  public void FindByPointで特定のポイントのデータが取得されること() {
+    List<Ramen> ramens = ramenMapper.getPoint(9);
+    assertNotNull(ramens);
+    assertEquals(1, ramens.size());
+  }
+
+  @Test
+  @DataSet(cleanBefore = true)
+  public void ラーメンデータが登録できること() {
+    Ramen ramen = new Ramen(1, "カドヤ食堂", "大阪 西長堀", 980, 9);
+    ramenMapper.create(ramen);
+    assertNotNull(ramen.getId());
+  }
+
+  @Test
+  @DataSet(value = {"datasets/update.yml"})
+  @ExpectedDataSet(value = "expectedAfterUpdateRamens.yml")
+  public void ラーメンデータが更新できること() {
+    ramenMapper.update(1, "カドヤ食堂", "大阪 西長堀", 980, 9);
+  }
+
+  @Test
+  @DataSet(value = {"datasets/ramen.yml"})
+  @ExpectedDataSet(value = "expectedAfterDeleteRamens.yml")
+  public void ラーメンデータが消去できること() {
+    assertThat(ramenMapper.findAll()).hasSize(10);
+    ramenMapper.delete(1);
+    assertThat(ramenMapper.findAll()).hasSize(9);
+
+  }
+}
+
+
+
---
+++ src/test/resources/sqlannotation/insert-ramens.sql
@@ -0,0 +1,10 @@
+INSERT INTO ramens (id, name, address, ave_price, point) VALUES (1, "カドヤ食堂","大阪 西長堀",980,9);
+INSERT INTO ramens (id, name, address, ave_price, point) VALUES (2, "人類みな麺類","大阪 西中島南方",800, 10);
+INSERT INTO ramens (id, name, address, ave_price, point) VALUES (3, "NOROMA","奈良 帯解",850,10);
+INSERT INTO ramens (id, name, address, ave_price, point) VALUES (4, "88","奈良 天理",850,6);
+INSERT INTO ramens (id, name, address, ave_price, point) VALUES (5, "たけ井","京都 城陽",800,10);
+INSERT INTO ramens (id, name, address, ave_price, point) VALUES (6, "俺のラーメンあっぱれ屋","京都 山城",1000,8);
+INSERT INTO ramens (id, name, address, ave_price, point) VALUES (7, "麺魚","東京 錦糸町",1000,10);
+INSERT INTO ramens (id, name, address, ave_price, point) VALUES (8, "一燈","東京 新小岩",1000,10);
+INSERT INTO ramens (id, name, address, ave_price, point) VALUES (9, "風神","東京 品川",640,2);
+INSERT INTO ramens (id, name, address, ave_price, point) VALUES (10, "千華","京都 福知山",700,5);
