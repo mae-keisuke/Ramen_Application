60382a513d0a983bebdb264d0512546341fc0ee5
mae <dup0729@gmail.com>
2023-04-17 09:52:32 +0900

    全データが取得できる結合テストが成功するように修正

--- src/test/java/com/raisetech/mybatis/integrationtest/RamenRestApiIntegrationTest.java
+++ src/test/java/com/raisetech/mybatis/integrationtest/RamenRestApiIntegrationTest.java
@@ -38,72 +38,72 @@ public class RamenRestApiIntegrationTest {
                 "id": 1,
                 "name": "カドヤ食堂",
                 "address": "大阪 西長堀",
-                "ave_price": 980,
+                "avePrice": 980,
                 "point": 9
             },
             {
                 "id": 2,
                 "name": "人類みな麺類",
                 "address": "大阪 西中島南方",
-                "ave_price": 800,
+                "avePrice": 800,
                 "point": 10
             },
             {
                 "id": 3,
                 "name": "NOROMA",
                 "address": "奈良 帯解",
-                "ave_price": 850,
+                "avePrice": 850,
                 "point": 10
             },
             {
                 "id": 4,
                 "name": "88",
                 "address": "奈良 天理",
-                "ave_price": 850,
+                "avePrice": 850,
                 "point": 6
             },
             {
                 "id": 5,
                 "name": "たけ井",
                 "address": "京都 城陽",
-                "ave_price": 800,
+                "avePrice": 800,
                 "point": 10
             },
             {
                 "id": 6,
                 "name": "俺のラーメンあっぱれ屋",
                 "address": "京都 山城",
-                "ave_price": 1000,
+                "avePrice": 1000,
                 "point": 8
             },
             {
                 "id": 7,
                 "name": "麺魚",
                 "address": "東京 錦糸町",
-                "ave_price": 1000,
+                "avePrice": 1000,
                 "point": 10
             },
             {
                 "id": 8,
                 "name": "一燈",
                 "address": "東京 新小岩",
-                "ave_price": 1000,
+                "avePrice": 1000,
                 "point": 10
             },
             {
                 "id": 9,
                 "name": "風神",
                 "address": "東京 品川",
-                "ave_price": 640,
+                "avePrice": 640,
                 "point": 2
             },
             {
                 "id": 10,
                 "name": "千華",
                 "address": "京都 福知山",
-                "ave_price": 700,
+                "avePrice": 700,
                 "point": 5
-            },
+            }
         ]
         """, response, JSONCompareMode.STRICT
     );
